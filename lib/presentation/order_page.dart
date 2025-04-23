1ef3b7d951a75642bc38c3755e31786a301dc742
Bayhaqiindra <bayhaqi30072004@gmail.com>
2025-04-23 16:16:24 +0700

    Menambahkan input pesanan minuman dan jumlah makanan di OrderPage

--- lib/presentation/order_page.dart
+++ lib/presentation/order_page.dart
@@ -53,3 +53,13 @@ class _OrderPageState extends State<OrderPage> {
                 return null;
               },
             ),
+            TextFormField(
+              controller: jumlahMakananController,
+              decoration: const InputDecoration(labelText: 'Food QTY Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your qty of food order';
+                }
+                return null;
+              },
+            ),
