4c3d7a53d7f12dd6acf686eab175bed4c5e968d6
Bayhaqiindra <bayhaqi30072004@gmail.com>
2025-04-23 16:04:50 +0700

    Membuat UI home tombol Order Now yang navigasi ke OrderPage

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -7,3 +7,29 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Selamat datang di Home Page!'),
+            const SizedBox(height: 8), // Replaced 'spacing' with SizedBox
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OrderPage()),
+                );
+              },
+              child: const Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
