843092b7c64243598f2f2b66e9c8e2cc3710a4eb
Bayhaqiindra <bayhaqi30072004@gmail.com>
2025-04-23 15:52:54 +0700

    Buat halaman registrasi dengan form email dan password

---
+++ lib/presentation/register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  
